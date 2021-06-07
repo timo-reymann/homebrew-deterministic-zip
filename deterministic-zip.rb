561a3722d191d2f9aa9a450280016fba8a899226
CircleCI <no-reply@circleci.com>
2021-06-07 11:27:06 +0000

    chore: Update formula for version 0.0.11

--- deterministic-zip.rb
+++ deterministic-zip.rb
@@ -1,11 +1,10 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# Documentation: https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 
 class DeterministicZip < Formula
     desc "Simple (almost drop-in) replacement for zip that produces deterministic files."
-    url "https://github.com/timo-reymann/deterministic-zip/releases/download/0.0.10/deterministic-zip_darwin-amd64"
-    sha256 "4d6b14bb8af0e1e61e777d1baf3037bc392d7648640baca4daee542da9ce63ad"
-    version "0.0.10"
+    url "https://github.com/timo-reymann/deterministic-zip/releases/download/0.0.11/deterministic-zip_darwin-amd64"
+    sha256 "5fe0a9fd13300a9f5f032844e50093a3f991ced95ea8a8e4e8f92101cf6fc33d"
+    version "0.0.11"
 
     def install
         bin.install "deterministic-zip_darwin-amd64" => "deterministic-zip"
