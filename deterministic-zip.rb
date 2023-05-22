d99372ea86a17669cb5cd10acf3bb8bdf6792d0b
CircleCI <no-reply@circleci.com>
2023-05-22 09:35:27 +0000

    chore: Update formula for version 2.0.1

--- deterministic-zip.rb
+++ deterministic-zip.rb
@@ -2,9 +2,9 @@
 
 class DeterministicZip < Formula
     desc "Simple (almost drop-in) replacement for zip that produces deterministic files."
-    url "https://github.com/timo-reymann/deterministic-zip/releases/download/2.0.0/deterministic-zip_darwin-amd64"
-    sha256 "bfa4dc0aa5b2baf56580fb26049d07ecfd3bbcfaa25b717411080a87bd20f78c"
-    version "2.0.0"
+    url "https://github.com/timo-reymann/deterministic-zip/releases/download/2.0.1/deterministic-zip_darwin-amd64"
+    sha256 "877c09f81888641b26f39e1619a1b886aa924ecd9c95d47aede6aab9aeb560a5"
+    version "2.0.1"
 
     def install
         bin.install "deterministic-zip_darwin-amd64" => "deterministic-zip"
