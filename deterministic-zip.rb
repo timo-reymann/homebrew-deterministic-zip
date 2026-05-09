b6fda01291d10f15ebf3e7f07addeb7fbc2fc98e
Timo Reymann <mail@timo-reymann.de>
2026-05-09 17:09:30 +0200

    feat: Add ARM support

--- deterministic-zip.rb
+++ deterministic-zip.rb
@@ -2,12 +2,25 @@
 
 class DeterministicZip < Formula
     desc "Simple (almost drop-in) replacement for zip that produces deterministic files."
-    url "https://github.com/timo-reymann/deterministic-zip/releases/download/6.0.3/deterministic-zip_darwin-amd64"
-    sha256 "7a21916b3f400fd93568251ee74613c510a9218eb0f5175aae2e7567f7896d39"
     version "6.0.3"
 
+    on_macos do
+      on_intel do
+        url "https://github.com/timo-reymann/deterministic-zip/releases/download/6.0.3/deterministic-zip_darwin-amd64"
+        sha256 "7a21916b3f400fd93568251ee74613c510a9218eb0f5175aae2e7567f7896d39"
+      end
+      on_arm do
+        url "https://github.com/timo-reymann/deterministic-zip/releases/download/6.0.3/deterministic-zip_darwin-arm64"
+        sha256 "2ab41c6b0def889a000067298bc855cb2e315295551aa136dd40340503629858"
+      end
+    end
+
     def install
-        bin.install "deterministic-zip_darwin-amd64" => "deterministic-zip"
+        if Hardware::CPU.arm?
+            bin.install "deterministic-zip_darwin-arm64" => "deterministic-zip"
+        else
+            bin.install "deterministic-zip_darwin-amd64" => "deterministic-zip"
+        end
     end
 
     test do
--- generate.py
+++ generate.py
@@ -4,13 +4,15 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description="Create homebrew formula from template")
 parser.add_argument("--version", help="Version to set", required=True)
-parser.add_argument("--sha256-mac", help="SHA512 checksum for macos binary", required=True)
+parser.add_argument("--sha256-mac-amd64", help="SHA256 checksum for macOS amd64 binary", required=True)
+parser.add_argument("--sha256-mac-arm64", help="SHA256 checksum for macOS arm64 binary", required=True)
 
 args = parser.parse_args()
 
 data = {
     "version": args.version,
-    "sha256_mac": args.sha256_mac,
+    "sha256_mac_amd64": args.sha256_mac_amd64,
+    "sha256_mac_arm64": args.sha256_mac_arm64,
 }
 
 formula_template = None
